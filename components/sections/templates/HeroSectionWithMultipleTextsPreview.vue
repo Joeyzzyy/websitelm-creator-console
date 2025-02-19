abdb2a53c89deec0554b15bc9e5812b9234dcb41
Yue Zhu <zhuyuejoey@gmail.com>
2025-02-19 16:41:41 +0800

    features added

--- components/sections/templates/HeroSectionWithMultipleTextsPreview.vue
+++ components/sections/templates/HeroSectionWithMultipleTextsPreview.vue
@@ -21,11 +21,11 @@
             {{ section.topContent.subTitle }}
           </p>
 
-          <div class="pt-6 flex justify-center gap-3 flex-wrap">
+          <div class="pt-6 flex justify-center gap-2 flex-wrap">
             <button 
               v-if="section.topContent.showButton"
               @click="handleButtonClick('demo')"
-              class="px-8 py-3 rounded-xl text-base font-semibold transition-all duration-200 bg-transparent text-white border-2 border-white hover:scale-105"
+              class="px-4 py-2 rounded-lg text-sm font-semibold transition-all duration-200 bg-transparent text-white border-2 border-white hover:scale-105"
             >
               {{ section.topContent.buttonText }}
             </button>
@@ -33,7 +33,7 @@
             <button 
               v-if="section.topContent.showCtaButton"
               @click="handleButtonClick('getStarted')"
-              class="px-8 py-3 rounded-xl text-base font-semibold transition-all duration-200 bg-[#3374FF] text-white hover:scale-105"
+              class="px-4 py-2 rounded-lg text-sm font-semibold transition-all duration-200 bg-[#3374FF] text-white hover:scale-105"
             >
               {{ section.topContent.ctaButtonText }}
             </button>
@@ -43,9 +43,9 @@
               v-if="section.topContent.enableDiscord && section.topContent.discordLink"
               :href="section.topContent.discordLink"
               target="_blank"
-              class="inline-flex items-center px-6 py-3 rounded-xl text-base font-semibold transition-all duration-200 bg-[#5865F2] text-white hover:bg-[#4752C4] hover:scale-105 border-2 border-white"
+              class="inline-flex items-center px-4 py-2 rounded-lg text-sm font-semibold transition-all duration-200 bg-[#5865F2] text-white hover:bg-[#4752C4] hover:scale-105 border-2 border-white"
             >
-              <svg class="w-6 h-6 mr-2" viewBox="0 0 24 24" fill="currentColor">
+              <svg class="w-4 h-4 mr-2" viewBox="0 0 24 24" fill="currentColor">
                 <path d="M20.317 4.37a19.791 19.791 0 0 0-4.885-1.515a.074.074 0 0 0-.079.037c-.21.375-.444.864-.608 1.25a18.27 18.27 0 0 0-5.487 0a12.64 12.64 0 0 0-.617-1.25a.077.077 0 0 0-.079-.037A19.736 19.736 0 0 0 3.677 4.37a.07.07 0 0 0-.032.027C.533 9.046-.32 13.58.099 18.057a.082.082 0 0 0 .031.057a19.9 19.9 0 0 0 5.993 3.03a.078.078 0 0 0 .084-.028a14.09 14.09 0 0 0 1.226-1.994a.076.076 0 0 0-.041-.106a13.107 13.107 0 0 1-1.872-.892a.077.077 0 0 1-.008-.128a10.2 10.2 0 0 0 .372-.292a.074.074 0 0 1 .077-.01c3.928 1.793 8.18 1.793 12.062 0a.074.074 0 0 1 .078.01c.12.098.246.198.373.292a.077.077 0 0 1-.006.127a12.299 12.299 0 0 1-1.873.892a.077.077 0 0 0-.041.107c.36.698.772 1.362 1.225 1.993a.076.076 0 0 0 .084.028a19.839 19.839 0 0 0 6.002-3.03a.077.077 0 0 0 .032-.054c.5-5.177-.838-9.674-3.549-13.66a.061.061 0 0 0-.031-.03zM8.02 15.33c-1.183 0-2.157-1.085-2.157-2.419c0-1.333.956-2.419 2.157-2.419c1.21 0 2.176 1.096 2.157 2.42c0 1.333-.956 2.418-2.157 2.418zm7.975 0c-1.183 0-2.157-1.085-2.157-2.419c0-1.333.955-2.419 2.157-2.419c1.21 0 2.176 1.096 2.157 2.42c0 1.333-.946 2.418-2.157 2.418z"/>
               </svg>
               Join Discord
@@ -60,9 +60,9 @@
               <img 
                 :src="`https://api.producthunt.com/widgets/embed-image/v1/featured.svg?post_id=${section.topContent.productHuntId}&theme=light`" 
                 :alt="`${section.topContent.productHuntId} - Featured on Product Hunt`" 
-                style="width: 250px; height: 54px;"
-                width="250"
-                height="54"
+                style="width: 180px; height: 40px;"
+                width="180"
+                height="40"
               />
             </a>
           </div>
